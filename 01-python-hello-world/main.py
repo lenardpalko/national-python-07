934623ec776ca6eefc36922a9339462d82f381e1
Lenard Palko <lenard.palko@gmail.com>
2021-11-14 22:03:29 +0200

    Added python-hello-world.

---
+++ 01-python-hello-world/main.py
@@ -0,0 +1,89 @@
+# asta este un comentariu
+# o nou linite de comment
+# print("Hello, Lenny!")
+
+# print("Hello, World!")  # este un print
+# print("text")
+# CTRL + /
+
+# print("My age is", 18, "years old")
+# print(20, type(20), type(20).__name__)
+# print(2.5, type(2.5), type(2.5).__name__)
+# print(3 + 5j, type(3 + 5j), type(3 + 5j).__name__)
+
+# print(int("20"))
+# print(float(20))
+# print(complex(20.50))
+
+# print(9/4)
+# print(9 // 4)
+# print(9 % 4)
+# print(2**2)
+
+# print(1 + 2 + 2.5)
+# print(1 + 2)
+
+# print(True)
+# print(False)
+
+# None
+
+# print(7 is None)
+# print(7 == 4)
+# print(7 == 7)
+
+# x = 7.5
+# print(x, type(x))
+
+# var1 = 7
+# var2 = 3
+# print(var1 != var2)
+# print(var1 < var2 or var1 == var2)
+
+# print(var1 is not var2)
+# print(1 not in [1, 2, 3])
+
+# print(bin(6 & 7))
+# print(bin(60))
+# print(bin(32))
+# print(bin(60&32))
+# print(bin(60|32))
+# print(60|32)
+
+# x = 5
+# print(type(x))
+# x = "string"
+# print(type(x))
+
+# my_age = 18
+# MY_AGE = 20
+# print(id(MY_AGE))
+# print(id(my_age))
+
+# my_name = "Lenny"
+# my_number = 20
+# print(id(my_name))
+# print(id(my_number))
+
+# my_number = my_number + 5
+# my_number += 5
+# print(my_number)
+
+# print(ord("a"), ord("z"))
+# print(chr(97), chr(65))
+
+# print("Numele meu este "Lenny"!")
+# print('Numele meu este "Lenny"!')
+# print("Numele meu este \"Lenny\"!")
+
+# print("This is the first line\nThis is the second line")
+# print("""This is the first line
+# This is the second line""")
+# print(r"This is the first line\nThis is the second line")
+
+nume = "Lenny"
+age = 35
+print("Numele meu este {name} si am {age} ani".format(age=35, name=nume))
+print("Numele meu este {0} si am {age} ani".format("Lenny", age=35))
+print(f"Numele meu este {nume} si am {age} ani")
+print("Numele meu este %s si am %d ani" % ("Lenny", 35))
