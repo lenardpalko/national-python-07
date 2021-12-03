73fffd7ebe7a3f410cef5bca7487e38cf71d81d3
Lenard Palko <lenard.palko@gmail.com>
2021-12-04 01:41:25 +0200

    Fixed test parametrization syntax.

--- 07-decorators-pytest/tests/test_functions.py
+++ 07-decorators-pytest/tests/test_functions.py
@@ -7,7 +7,7 @@ sequences = (
 )
 
 
-@pytest.mark.parametrize('sequence,expected_result',[
+@pytest.mark.parametrize('sequence,expected_result', [
     (sequences[0], 45),
     (sequences[1], 18),
 ])
@@ -16,9 +16,9 @@ def test_sum_from_args(sequence, expected_result):
     assert result == expected_result
 
 
-@pytest.mark.parametrize('sequence,expected_result',[
-    ((sequences[0], 15),
-    ((sequences[1], 6),
+@pytest.mark.parametrize('sequence,expected_result', [
+    (sequences[0], 15),
+    (sequences[1], 6),
 ])
 def test_original_sum_from_args():
     result = sum_from_args.__wrapped__(1, 2, 3, 4, 5)
